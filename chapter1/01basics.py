ff5b6af71fc8c1506df85f90b82c38c74e076a35
nawazishfaraz <nawazishfaraz14@gmail.com>
2024-12-02 02:03:21 +0530

    initial commit

---
+++ chapter1/01basics.py
@@ -0,0 +1,14 @@
+# comments in python:
+# comments in python are to write something that programmer doesnt want execute
+
+# two types of comments are there in python 
+
+#1 : single line: # is used for this purpose 
+
+
+#2: triple single quote is used for this purpose
+"""This is an amazing 
+example of a Multiline 
+comment!""" 
+
+print ("hello world")
