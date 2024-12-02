1369a462597533ed095b0504a4c6f6f4a94dfdd3
nawazishfaraz <nawazishfaraz14@gmail.com>
2024-12-02 09:43:37 +0530

    my second commit

---
+++ chapter2/02datatype.py
@@ -0,0 +1,15 @@
+#Primarily these are the following data types in Python: 
+#1. Integers 
+#2. Floating point numbers 
+#3. Strings 
+#4. Booleans  
+#5. None 
+#Python is a fantastic language that automatically identifies the type of data for us.
+
+a= 71         
+# identifies a as class <int> 
+
+b=88.44       
+# identifies b as class <float> 
+
+name= "harry" # identifies name as class <str>  
