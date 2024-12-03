27db5a5ba2ddd686b6409d20af48d1e3eb4ffdd7
nawazishfaraz <nawazishfaraz14@gmail.com>
2024-12-03 17:24:42 +0530

    my fourth commit2

--- chapter4/02tuple.py
+++ chapter4/02tuple.py
@@ -1,1 +1,8 @@
-print("hello world")
+
+#tuple is an immutable data type in python. 
+a = () # empty tuple 
+a = (1,) # tuple with only one element needs a comma 
+a = (1,7,2) # tuple with more than one element
+my_tuple = (10, 20, 30)
+print(my_tuple[0])  # Output: 10
+print(my_tuple[-1]) # Output: 30
